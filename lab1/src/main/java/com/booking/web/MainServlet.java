48af0f57c15d36038c99f724b4459479cd231725
Daria <124911987+dariaDor@users.noreply.github.com>
2025-12-17 01:01:27 +0400

    Create MainServlet.java

---
+++ lab1/src/main/java/com/booking/web/MainServlet.java
@@ -0,0 +1,26 @@
+package com.booking.web;
+
+import com.booking.ejb.BookingServiceBean;
+import com.booking.ejb.ClientServiceBean;
+import jakarta.ejb.EJB;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/")
+public class MainServlet extends HttpServlet {
+
+  @EJB
+  private BookingServiceBean bookingService;
+
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+          throws ServletException, IOException {
+      req.setAttribute("bookings", bookingService.getAllBookings());
+      req.getRequestDispatcher("/index.jsp").forward(req, resp);
+  }
+  
+}
